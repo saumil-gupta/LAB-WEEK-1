f5bd8e6816d2cfaab8508c8e0dfc1f672be29e1a
saumil-gupta <22103179@mail.jiit.ac.in>
2024-07-25 15:59:16 +0530

    Update P1.cpp

--- P1.cpp
+++ P1.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 int main()
 {
+    int size;
+    cout<<"ENTER THE SIZE OF THE ARRAY : ";
+    cin>>size;
+    int array[size];
+    cout<<"ENTER THE ELEMENTS OF THE ARRAY : ";
+    for(int i=0;i<=size-1;i++)
+    {
+        cin>>array[i];
+    }
+    int sum=0;
+    int avg=0;
+    for(int i=0;i<=size-1;i++)
+    {
+        sum+=array[i];
+    }
+    avg=sum/size;
+    cout<<"SUM IS "<<sum<<endl;
+    cout<<"AVERAGE IS "<<avg<<endl;
     return 0;
 }
